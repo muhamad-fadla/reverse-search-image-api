31864ae5c632abb66a9824eb6b17dc8611d47d89
muhamad-fadla <yuratamma@gmail.com>
2021-08-07 15:02:37 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const reverseImageSearch = require('node-reverse-image-search')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/reverse', async (req,res) => {
+
+	if(!req.query){
+		return res.status(500).json({
+			status: false,
+			message: "No url found"
+		});
+	}
+	if(typeof res.query['url'] == "undefined"){
+		return res.status(500).json({
+			status: false,
+			message: "No url found"
+		});
+	}
+
+	await reverseImageSearch(req.query['url'], function(data){
+		res.status(200).json({
+			status: true,
+			data: data
+		});
+	});
+
+
+});
+
+app.listen(PORT, 'localhost', function(){
+	console.log('Listening on', PORT);
+});
